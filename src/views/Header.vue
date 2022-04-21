6755adffe73b6f42b56e927aa743391709e2d417
MaryahKim <maryapitaz@gmail.com>
2022-04-21 12:28:07 +0300

    Create Header.vue
    create header  component

---
+++ src/views/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <header />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from '../components/Header.vue'
+
+export default {
+  name: 'Header',
+  components: {
+    Header
+   
+  }
+}
+</script>
