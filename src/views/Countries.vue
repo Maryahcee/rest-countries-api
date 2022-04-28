7f2b52c064d76fa00a66b9a4c1c5632d4f667ced
MaryahKim <maryapitaz@gmail.com>
2022-04-28 13:06:16 +0300

    Create Countries.vue
    Create countries component

---
+++ src/views/Countries.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <countries />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Countries from '../components/Countries.vue'
+
+export default {
+  name: 'Countries',
+  components: {
+   Countries
+   
+  }
+}
+</script>
