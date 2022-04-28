8e10ff500989ddd8a75ef965bf00628f54119d51
MaryahKim <maryapitaz@gmail.com>
2022-04-28 13:04:17 +0300

    Create fonticons.js
    Create Fonts folder

---
+++ src/fonticons.js
@@ -0,0 +1,8 @@
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faPhone);
+library.add(faUser);
+
+export default FontAwesomeIcon;
