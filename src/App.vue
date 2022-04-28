96d0869a02357daed4569edce2b939fcf7735533
MaryahKim <maryapitaz@gmail.com>
2022-04-28 13:00:03 +0300

    Update App.vue
    import components

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,26 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> 
+  <div>
+    <navbar />
+    <search-bar />
+    <countries />
+    <router-link to="/"></router-link> 
   </div>
   <router-view/>
 </template>
 
+<script>
+import Navbar from './components/Navbar.vue'
+import SearchBar from './components/SearchBar.vue'
+import Countries from './components/Countries.vue'
 
+export default ({
+  name: 'App',
+  components: {
+    Navbar,
+    SearchBar,
+    Countries,
+  }
+})
+</script>
+
+, SearchBar
