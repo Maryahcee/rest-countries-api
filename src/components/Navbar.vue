207467d6af5ac14769dc65f3748cf871c4a95f45
MaryahKim <maryapitaz@gmail.com>
2022-04-28 13:02:21 +0300

    Create Navbar.vue
    Create Navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav >
+    <div class="bg-white shadow-md container flex font-['Nunito Sans'] max-w-full">
+        <h1 class="m-5 font-bold text-lg float-left">Where in the World?</h1>
+      <div class="flex m-5 mx-auto mr-10">
+        <i  class="fa-solid fa-moon mr-2  w-10 h-10"></i>
+        <icon name="arrow-up" class="w-10 h-10" />
+        <font-awesome-icon icon="phone" />
+        <h3>Dark Mode</h3>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+
+export default {
+  name: 'Navbar',
+  
+
+}
+</script>
+
+<style>
+
+</style>
