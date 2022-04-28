facc747a42ac50bab24a742b22bb8da31d079a65
MaryahKim <maryapitaz@gmail.com>
2022-04-28 13:06:50 +0300

    Update tailwind.config.js
    Add custon Fonts and box shadow to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,12 @@ module.exports = {
         'Light-Mode-Input': 'hsl(0, 0%, 52%)',
         'Light-Mode-Background':' hsl(0, 0%, 98%)',
         'White': 'hsl(0, 0%, 100%)',
+      },
+      fonts: {
+       ' Nunito': ['Nunito Sans', 'Sans'],
+      },
+      boxShadow: {
+        '3xl': '0px 2px 9px rgba(0, 0, 0, 0.05)',
       }
     },
   },
